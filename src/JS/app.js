49be0f074369126d6d91544c67ff8e02fac3699a
Will Borysiak <will.gains@icloud.com>
2022-03-26 16:36:45 +0000

    Map and basic layout complete

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,1 +1,16 @@
-import '../SCSS-Files/main.scss';
+import '../SCSS/main.scss';
+
+export class App {
+	#map;
+	constructor() {
+		this.loadMap();
+	}
+
+	loadMap() {
+		// Load Map
+		this.#map = L.map('map').setView([51.505, -0.09], 12);
+		L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {}).addTo(this.#map);
+	}
+}
+
+const app = new App();
