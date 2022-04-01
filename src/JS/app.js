6547409b3b17eff5d7129784ff7b4c679623341e
Will Borysiak <will.gains@icloud.com>
2022-04-01 18:46:52 +0100

    markers added and css scrolling corrected

--- src/JS/app.js
+++ src/JS/app.js
@@ -4,36 +4,53 @@
 
 import '../SCSS/main.scss';
 import * as qS from './querySelectors.js';
-import { battleToggle } from './appFunctions.js';
 import jpg from '../images/ww2.jpg';
 
 class App {
+	map;
 	constructor() {
 		this.loadMap();
-		// this.battleView();
 		this.showInfoView();
 		this.hideInfoView();
 	}
 
 	loadMap() {
-		const latitude = 55;
-		const longitude = 17;
+		const latitude = 47;
+		const longitude = 13;
 		const coords = [latitude, longitude];
-		const map = L.map('map').setView(coords, 5);
+		const map = L.map('map').setView(coords, 4);
 
 		L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		}).addTo(map);
+
+		map.on('click', function (e) {
+			console.log(e.latlng.lat);
+			console.log(e.latlng.lng);
+		});
+
+		// Battle of the Atlantic
+		L.marker([51.120749121123474, -21.14596820310109]).addTo(map).bindPopup('Battle of the Atlantic');
+		// Battle of Britain
+		L.marker([51.45248705295193, -0.1490947363599138]).addTo(map).bindPopup('Battle of the Atlantic');
+		// Battle of Stalingrad
+		L.marker([48.71055093887703, 44.508198979739426]).addTo(map).bindPopup('Battle of Stalingrad');
+		// Battle of Kursk
+		L.marker([51.73804587385561, 36.17604894388018]).addTo(map).bindPopup('Battle of Kursk');
+		// D-Day
+		L.marker([49.35731764349393, -0.8457992109479218]).addTo(map).bindPopup('D-Day');
+		// Battle of the Bulge
+		L.marker([50.00271711751032, 5.717856776770534]).addTo(map).bindPopup('Battle of the Bulge');
+		// Battle of Berlin
+		L.marker([52.51820049405036, 13.399067612324352]).addTo(map).bindPopup('Battle of Berlin');
 	}
 
-	// battleView() {
-	// 	qS.testBtn.addEventListener('click', battleToggle);
-	// }
-
 	showInfoView() {
-		qS.battleItem.addEventListener('mouseover', function () {
+		console.log();
+		qS.battleItem.addEventListener('mouseover', function (e) {
 			qS.detailsContainer.classList.remove('details-container-hide');
 			qS.detailsContainer.classList.add('details-container-show');
+			// console.log(e.target);
 		});
 	}
 	hideInfoView() {
---
+++ src/JS/battlesArray.js
@@ -0,0 +1,22 @@
+const battleArr = [
+	// 3 Sept 1939 – 8 May 1945
+	'Battle of the Atlantic',
+
+	// 	10 July – 31 October 1940
+	'Battle of Britain',
+
+	// 23 Aug 1942 – 2 Feb 1943
+	'Battle of Stalingrad',
+
+	// 5 Jul 1943 – 23 Aug 1943
+	'Battle of Kursk',
+
+	// 6 June 1944
+	'D-Day',
+
+	// 16 Dec 1944 – 25 Jan 1945
+	'Battle of the Bulge',
+
+	// 16 Apr 1945 – 2 May 1945
+	'Battle of Berlin',
+];
