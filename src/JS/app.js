33d567bdf465328afb802035563944e70c8ecb22
Will Borysiak <will.gains@icloud.com>
2022-04-04 13:14:31 +0100

    main layout complete, styling tweaks added

--- src/JS/app.js
+++ src/JS/app.js
@@ -16,19 +16,16 @@ class App {
 
 	renderMap() {
 		// Map
-		const latitude = 47;
-		const longitude = 13;
-		const coords = [latitude, longitude];
-		const map = L.map('map').setView(coords, 4);
+		const map = L.map('map').setView([47, 13], 4);
 
 		L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 		}).addTo(map);
 
-		map.on('click', function (e) {
-			console.log(e.latlng.lat);
-			console.log(e.latlng.lng);
-		});
+		// map.on('click', function (e) {
+		// 	console.log(e.latlng.lat);
+		// 	console.log(e.latlng.lng);
+		// });
 
 		// Markers
 		battleData().forEach(battle => {
@@ -45,7 +42,7 @@ class App {
 			li.innerHTML = `
 			<div id="${battle.img_id}" class="battle-image"></div>
 						<h2 class="battle-title">${battle.name}</h2>
-						<div class="details-container">
+						<article class="details-container">
 							<div class="battle-details">
 								<span class="battle-info">Date</span>
 								<span class="battle-value">${battle.date}</span>
@@ -65,15 +62,28 @@ class App {
 						</div>
 					</div>
 			`;
+
 			// Toggle Data
 			li.addEventListener('click', function (e) {
+				// Variables
 				const details = li.querySelector('.details-container');
-				// Reset other battles
+				const image = li.querySelector('.battle-image');
+				const clickedBattle = e.target.closest('article');
+				// Reset data
 				document.querySelectorAll('.details-container-show').forEach(item => {
 					item.classList.remove('details-container-show');
 				});
-				details.classList.add('details-container-show');
+				// Reset images
+				document.querySelectorAll('.battle-image-blur').forEach(item => {
+					item.classList.remove('battle-image-blur');
+				});
+				// Show current battle
+				if (clickedBattle === null) {
+					details.classList.add('details-container-show');
+					image.classList.add('battle-image-blur');
+				}
 			});
+
 			// Insert Li to DOM
 			qS.battleContainer.appendChild(li);
 		});
